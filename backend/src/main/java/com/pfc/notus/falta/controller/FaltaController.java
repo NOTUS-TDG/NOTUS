29a42d114e76824fab55d1bb68b83713906681c5
Lucas <zakskk.4321@gmail.com>
2026-09-11 22:38:17 -0300

    Debugging classe falta

--- backend/src/main/java/com/pfc/notus/falta/controller/FaltaController.java
+++ backend/src/main/java/com/pfc/notus/falta/controller/FaltaController.java
@@ -22,7 +22,7 @@ public class FaltaController {
         return faltaService.getAllFaltas();
     }
 
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    @PreAuthorize("hasAnyRole('ROLE_PROFESSOR')")
     @GetMapping("/student/{studentId}")
     public List<FaltaDTO> getByStudent(@PathVariable Long studentId) {
         return faltaService.getByStudent(studentId);

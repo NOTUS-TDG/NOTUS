e8dd1a59588b9dae5cf2a825b0dd1dd2f5cb95e1
Gabriel de Alvarenga <gaalvarenga.dev@gmail.com>
2026-09-22 16:00:05 -0300

    Adiciona helper media() em lib/utils

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function media(valores: number[]) {
+  return valores.reduce((a, b) => a + b, 0) / valores.length;
+}
